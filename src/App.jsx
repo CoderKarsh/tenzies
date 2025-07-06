c654a259e5e345fcc182f57d17a8a9f6e7b4ff1f
CoderKarsh <shreeut2704@gmail.com>
2025-07-07 00:48:00 +0530

    Add end game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import {nanoid} from 'nanoid'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Confetti from 'react-confetti'
 import Die from './Die'
 
 export default function App(){
   // callback function for lazy initialization otherwise generateAllDie will run at every re-render (Althogh it won't re-initialize dice)
   const [dice, setDice] = useState(() => generateAllDie())
+  const [windowDimensions, setWindowDimensions] = useState({
+			width: window.innerWidth,
+			height: window.innerHeight,
+		})
 
+
+    // Needed since react-confetti fails to render properly when window dimensions change
+    useEffect(() => {
+      window.addEventListener("resize", ()=>{
+        setWindowDimensions({
+          width: window.innerWidth,
+			    height: window.innerHeight,
+        })
+      })
+    }, [])
 
   // Creates a new array of size 10, fills each entry with 0 (initially null, required since otherwise they can't be iterated over) and then iterated over each element to fill them with a random values.
   function generateAllDie() {
@@ -18,6 +33,9 @@ export default function App(){
       }))
   }
 
+  const gameWon = dice.every(die => die.isHeld) &&
+    dice.every(die => die.value === dice[0].value)
+
   // Toggle isHeld state when user clicks on a die
   function toggleHold(id) {
     setDice(prevDice => prevDice.map(die => 
@@ -26,7 +44,8 @@ export default function App(){
   }
 
   // Randomize each non-held state
-  function roll() {
+  function handleClick() {
+    gameWon ? setDice(generateAllDie):
     setDice(prevDice => prevDice.map(die => (
       die.isHeld ? die : {...die, value: Math.floor(Math.random() * 6) + 1}
     )))
@@ -34,14 +53,18 @@ export default function App(){
   }
 
 
+  gameWon && console.log("Game Won!")
   return (
     <main>
+
+      {gameWon && <Confetti width={windowDimensions.width} height={windowDimensions.height}/>}
+
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className='dice-container'>
-        {dice.map(die => <Die key={die.id} dieObj={die} handleClick={() => toggleHold(die.id)}></Die>)}
+        {dice.map(die => <Die key={die.id} dieObj={die} handleToggle={() => toggleHold(die.id)}></Die>)}
       </section>
-      <button type='submit' className="roll-btn" onClick={roll}>Roll</button>
+      <button type='submit' className="roll-btn" onClick={handleClick}>{gameWon ? "Reset" : "Roll"}</button>
     </main>
   )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,6 +1,6 @@
-export default function Die ({dieObj, handleClick}) {
+export default function Die ({dieObj, handleToggle}) {
     return (
-    <button type="submit" className={`die-btn ${dieObj.isHeld ? "held-btn" : null}` } id={dieObj.id} onClick={handleClick}>
+    <button type="submit" className={`die-btn ${dieObj.isHeld ? "held-btn" : null}` } id={dieObj.id} onClick={handleToggle}>
         {dieObj.value}
     </button>)
 }
