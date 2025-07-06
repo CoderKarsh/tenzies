6a70c516369d6d2968b1415d55b6ab166265d86b
CoderKarsh <shreeut2704@gmail.com>
2025-07-07 00:23:43 +0530

    Make minor UI changes and finish main game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,47 @@
-import { useState } from 'react'
 import {nanoid} from 'nanoid'
+import { useState } from 'react'
 import Die from './Die'
+
 export default function App(){
+  // callback function for lazy initialization otherwise generateAllDie will run at every re-render (Althogh it won't re-initialize dice)
   const [dice, setDice] = useState(() => generateAllDie())
 
+
+  // Creates a new array of size 10, fills each entry with 0 (initially null, required since otherwise they can't be iterated over) and then iterated over each element to fill them with a random values.
   function generateAllDie() {
     return new Array(10)
-                .fill(0)
-                .map(() => Math.floor(Math.random() * 6) + 1)
+      .fill(0)
+      .map(() => ({
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid()
+      }))
   }
 
-  console.log(dice)
+  // Toggle isHeld state when user clicks on a die
+  function toggleHold(id) {
+    setDice(prevDice => prevDice.map(die => 
+      die.id === id ? {...die, isHeld: !die.isHeld } : die
+    ))
+  }
+
+  // Randomize each non-held state
+  function roll() {
+    setDice(prevDice => prevDice.map(die => (
+      die.isHeld ? die : {...die, value: Math.floor(Math.random() * 6) + 1}
+    )))
+    
+  }
+
 
   return (
     <main>
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className='dice-container'>
-        {dice.map(value => <Die key={nanoid()} value={value}></Die>)}
+        {dice.map(die => <Die key={die.id} dieObj={die} handleClick={() => toggleHold(die.id)}></Die>)}
       </section>
-      <button type='submit' className="roll-btn">Roll</button>
+      <button type='submit' className="roll-btn" onClick={roll}>Roll</button>
     </main>
   )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,6 +1,6 @@
-export default function Die ({value}) {
+export default function Die ({dieObj, handleClick}) {
     return (
-    <button type="submit" className='die-btn'>
-        {value}
+    <button type="submit" className={`die-btn ${dieObj.isHeld ? "held-btn" : null}` } id={dieObj.id} onClick={handleClick}>
+        {dieObj.value}
     </button>)
 }
