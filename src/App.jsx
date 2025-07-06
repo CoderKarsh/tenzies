6c065350349eb14d6ba42e68c2b901f7850cf571
CoderKarsh <shreeut2704@gmail.com>
2025-07-06 22:59:32 +0530

    Add structure of the web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react'
+import {nanoid} from 'nanoid'
+import Die from './Die'
 export default function App(){
+  const [dice, setDice] = useState(() => generateAllDie())
+
+  function generateAllDie() {
+    return new Array(10)
+                .fill(0)
+                .map(() => Math.floor(Math.random() * 6) + 1)
+  }
+
+  console.log(dice)
+
   return (
-    <main></main>
+    <main>
+      <h1>Tenzies</h1>
+      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <section className='dice-container'>
+        {dice.map(value => <Die key={nanoid()} value={value}></Die>)}
+      </section>
+      <button type='submit' className="roll-btn">Roll</button>
+    </main>
   )
 }
---
+++ src/Die.jsx
@@ -0,0 +1,6 @@
+export default function Die ({value}) {
+    return (
+    <button type="submit" className='die-btn'>
+        {value}
+    </button>)
+}
